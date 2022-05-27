20d8870c11e893ffe567aa0fc82e7425b5e6b9eb
CarsonWebster <CarsonWebster@hotmail.com>
2022-05-27 16:46:04 -0700

    Added filters mainly

--- components/filterBox.vue
+++ components/filterBox.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div class="flex items-center">
         <ul>
             <li><h1>Filters</h1></li>
@@ -21,5 +21,37 @@
             </li>
         </ul>
         
+    </div>
+</template> -->
+<template>
+    <div class="">
+        <div class="flex">
+            <!-- Filter svg -->
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+            <h1>Filters</h1>
+        </div>
+        <!-- Filters Form -->
+        <div class="form-control">
+            <label class="label cursor-pointer">
+                <span class="label-text">Engneering</span> 
+                <input type="checkbox" class="toggle toggle-primary" checked />
+            </label>
+            <label class="label cursor-pointer">
+                <span class="label-text">Science</span> 
+                <input type="checkbox" class="toggle toggle-primary" checked />
+            </label>
+            <label class="label cursor-pointer">
+                <span class="label-text">Theater</span> 
+                <input type="checkbox" class="toggle toggle-primary" checked />
+            </label>
+            <label class="label cursor-pointer">
+                <span class="label-text">Acting</span> 
+                <input type="checkbox" class="toggle toggle-primary" checked />
+            </label>
+            <label class="label cursor-pointer">
+                <span class="label-text">Broadway</span> 
+                <input type="checkbox" class="toggle toggle-primary" checked />
+            </label>
+        </div>
     </div>
 </template>
--- components/postCard.vue
+++ components/postCard.vue
@@ -17,7 +17,7 @@
         </div>
         <!-- Name and themes -->
         <div class="ml-1.5 text-sm leading-tight">
-          <span class="text-black dark:text-white font-bold block ">Carson Webster</span>
+          <span class="text-black dark:text-white font-bold block">Carson Webster</span>
           <div class="">
             <!-- <span class="text-gray-500 dark:text-gray-400 font-normal block">Themes buttons</span> -->
             <button class="btn btn-primary btn-xs">Engineering</button>
